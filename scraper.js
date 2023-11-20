1a8e4db75c22d54a0103f7ad2e25da0daba6b1ef
win <trieu1852000@gmail.com>
2023-11-20 11:26:07 -0500

    add description and director into rokuLink

--- scraper.js
+++ scraper.js
@@ -18,27 +18,42 @@ async function performScraping() {
         
         //console.log(axiosResponse.data)    //(finding property for axiosResponse.data below)
         const data = axiosResponse.data
-      
-
-        
         const numMovies = data.view.length;
         console.log(`Total Movies: ${numMovies}`);
         
         // Loop through all movies and collect data
         for (let i = 0; i < numMovies; i++) {
-            
             const movieData = data.view[i].content;
             const channelIdArray = movieData.viewOptions.map(option => option.channelId);
             const contentId = channelIdArray.includes("151908") ? movieData.meta.id : "";
-            const rokuLink = channelIdArray.includes("151908") ? `https://therokuchannel.roku.com/details/${contentId}`: "";
+            // skip the movie that no valid contentId
+            if (!contentId) {
+                //console.log(`Skipping movie: ${movieData.title} (No valid contentId)`);
+                continue;
+            }
+        
+            const rokuLink = contentId ? `https://therokuchannel.roku.com/details/${contentId}` : "";
 
+            console.log(`Processing movie: ${movieData.title}, contentId: ${contentId}`);
+            
+            // Add code to fetch director data
+            const moreResponse = await axios.request({
+                method:"GET",
+                accept: "application/json",
+                url:`https://www.roku.com/api/v1/sow/content/v1/roku/${contentId}`
+            });
+            const directorData = moreResponse.data.view.credits.find(credit => credit.role === "DIRECTOR");
+            const directorName = directorData ? directorData.name : "";
+
+            // Extract the description from moreResponse
+            const description = moreResponse.data.view.description;
 
             const rokuItem = {
                 "contentId": contentId,
                 "title": movieData.title,
-                "description": "",
+                "description": description,
                 "releaseDate": movieData.extra,
-                "directors": "",
+                "directors": directorName, 
                 "rokuLink": rokuLink,
                 "sourceId": 1237,
                 "device": "R"
