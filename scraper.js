4d848f1d3f08477321fc959f73f179255ca37c12
win <trieu1852000@gmail.com>
2023-11-20 11:49:16 -0500

    change axious to puppeteer To scrape content from multiple pages of a website that uses dynamic loading

--- scraper.js
+++ scraper.js
@@ -1,83 +1,76 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
-const fs = require("fs");
-
-
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 async function performScraping() {
+    // Launch Puppeteer and create a new page
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const rokuItems = [];
 
     try {
-        // downloading the target web page
-        // by performing an HTTP GET request in Axios
-        const rokuItems = []
-        const axiosResponse = await axios.request({
-            method: "GET",
-            accept: "application/json",
-            url:"https://www.roku.com/api/v1/sow/search"
-        })
-        
-        //console.log(axiosResponse.data)    //(finding property for axiosResponse.data below)
-        const data = axiosResponse.data
-        const numMovies = data.view.length;
-        console.log(`Total Movies: ${numMovies}`);
-        
-        // Loop through all movies and collect data
-        for (let i = 0; i < numMovies; i++) {
-            const movieData = data.view[i].content;
-            const channelIdArray = movieData.viewOptions.map(option => option.channelId);
-            const contentId = channelIdArray.includes("151908") ? movieData.meta.id : "";
-            // skip the movie that no valid contentId
-            if (!contentId) {
-                //console.log(`Skipping movie: ${movieData.title} (No valid contentId)`);
-                continue;
+        for (let pageNum = 1; pageNum <= 2; pageNum++) {
+            // Construct the URL for the Roku API page
+            const pageUrl = `https://www.roku.com/api/v1/sow/search?page=${pageNum}`;
+            await page.goto(pageUrl, { waitUntil: 'networkidle0' }); // Load the page
+
+            // Get the page content and parse it using Cheerio
+            const pageContent = await page.content();
+            const $ = cheerio.load(pageContent);
+
+            // Parse the JSON data embedded in the page
+            const data = JSON.parse($('body').text());
+
+            // Get the number of movies on the current page
+            const numMovies = data.view.length;
+            console.log(`Total Movies on Page ${pageNum}: ${numMovies}`);
+
+            // Loop through each movie on the page and collect data
+            for (let i = 0; i < numMovies; i++) {
+                const movieData = data.view[i].content;
+                const channelIdArray = movieData.viewOptions.map(option => option.channelId);
+                const contentId = channelIdArray.includes("151908") ? movieData.meta.id : "";
+
+                if (!contentId) {
+                    continue; // Skip movies without a valid contentId
+                }
+
+                const rokuLink = contentId ? `https://therokuchannel.roku.com/details/${contentId}` : "";
+
+                // Fetch additional details from the content API
+                const moreResponse = await page.goto(`https://www.roku.com/api/v1/sow/content/v1/roku/${contentId}`, { waitUntil: 'networkidle0' });
+                const moreResponseData = JSON.parse(await moreResponse.text());
+
+                // Find the director's name in the additional data
+                const directorData = moreResponseData.view.credits.find(credit => credit.role === "DIRECTOR");
+                const directorName = directorData ? directorData.name : "";
+
+                // Extract the movie description from the additional data
+                const description = moreResponseData.view.description;
+
+                // Create an object with the collected data and add it to the list
+                const rokuItem = {
+                    "contentId": contentId,
+                    "title": movieData.title,
+                    "description": description,
+                    "releaseDate": movieData.extra,
+                    "directors": directorName,
+                    "rokuLink": rokuLink,
+                    "sourceId": 1237,
+                    "device": "R"
+                };
+                rokuItems.push(rokuItem);
             }
-        
-            const rokuLink = contentId ? `https://therokuchannel.roku.com/details/${contentId}` : "";
-
-            console.log(`Processing movie: ${movieData.title}, contentId: ${contentId}`);
-            
-            // Add code to fetch director data
-            const moreResponse = await axios.request({
-                method:"GET",
-                accept: "application/json",
-                url:`https://www.roku.com/api/v1/sow/content/v1/roku/${contentId}`
-            });
-            const directorData = moreResponse.data.view.credits.find(credit => credit.role === "DIRECTOR");
-            const directorName = directorData ? directorData.name : "";
-
-            // Extract the description from moreResponse
-            const description = moreResponse.data.view.description;
-
-            const rokuItem = {
-                "contentId": contentId,
-                "title": movieData.title,
-                "description": description,
-                "releaseDate": movieData.extra,
-                "directors": directorName, 
-                "rokuLink": rokuLink,
-                "sourceId": 1237,
-                "device": "R"
-            };
-            rokuItems.push(rokuItem);
         }
+
+        // Write the collected data to a JSON file
         fs.writeFileSync('titles.json', JSON.stringify(rokuItems, null, 2));
-
-        //console.log(rokuItem.title)
-        //var fileCount
-        //fs.readdir("./data", (err, file) => {
-          //  fileCount = file.length 
-         //   console.log(fileCount)
-        //})
-        //console.log(fileCount)
-        //fs.writeFile(`./data/titles${fileCount}.json`.toString(), JSON.stringify(rokuItems, null, 2));
-
-
-        //console.log(axiosResponse.data['feed/search?movies=1'][0].view)
-        //console.log(axiosResponse.request)
         console.log('Data has been extracted and saved to titles.json');
-        
     } catch (error) {
         console.error("Error:", error);
+    } finally {
+        // Close the Puppeteer browser when done
+        await browser.close();
     }
 }
 
